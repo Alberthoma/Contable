915ed66508c1fe4b99f5c474ae65433637a9ef99
Alberthoma <you@example.com>
2025-10-30 22:43:23 -0400

    Actualizacion automatica 10/30/2025 22:43:23,19

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js: cache básico para PWA
+const CACHE = "contable-cache-v1";
+const ASSETS = ["./", "./index.html"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request))
+  );
+});
